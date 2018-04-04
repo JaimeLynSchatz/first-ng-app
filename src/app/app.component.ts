403c4237016b94222f8f7e2512eb2ede478df5b4
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2018-04-04 09:02:51 -0700

    playing with favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Angular (Took you long enough!)';
+  title = 'Hello Angular! (Took you long enough!)';
 }
